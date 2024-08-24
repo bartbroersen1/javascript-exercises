fb93515434726d4da014ad2a289bfa264eca99a5
Bart Broersen <143587354+bartbroersen1@users.noreply.github.com>
2024-08-24 16:02:33 +0200

    failed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numRemove) {
+    for (i = 0; i < numRemove.length; i++) {
+        while (array.includes(numRemove[i])) {
+        array.splice(numRemove[i] - 1, 1);
+        if (numRemove.length = 0) break;
+        };
+    }
+    return array;
 };
 
 // Do not edit below this line
