0f23e70ea5b6744a826af01c4692c83d368bdc3a
Bart Broersen <143587354+bartbroersen1@users.noreply.github.com>
2024-08-25 16:03:30 +0200

    solved 5 tests failed 2

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {  
+    
+    let numLow;
+    let numHigh;
+    const numArray = [];
+    let sum = 0;
 
+    if (!Number.isInteger(numOne || numTwo)
+        || numOne < 0 || numTwo < 0) {
+        return "ERROR";
+    }
+    else if (numOne < numTwo) {
+        numLow = numOne;
+        numHigh = numTwo;
+    }
+    else if (numOne > numTwo) {
+        numLow = numTwo;
+        numHigh = numOne;
+    }
+    else {
+        return numOne;
+    }
+
+    for (i = numLow; i <= numHigh; i++) {
+        numArray.push(i);
+    }
+
+    for (i = 0; i < numArray.length; i++) {
+        sum += numArray[i];
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
