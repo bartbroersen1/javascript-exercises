4160604c1696043d95a29cdb281c36f4724a25c1
Bart Broersen <143587354+bartbroersen1@users.noreply.github.com>
2024-09-26 22:31:40 +0200

    solved exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let array = string
+    .replace( /[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    .replace(/\s/g, '')
+    .toLowerCase()
+    .split("");
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== (array[array.length - 1 - i])) {
+            return false;
+        }
+    };
+    return true;
 };
 
 // Do not edit below this line
