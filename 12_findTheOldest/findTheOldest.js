f80c53db6384a7ed0b39073615f4bfc7be2c3db0
Bart Broersen <143587354+bartbroersen1@users.noreply.github.com>
2024-09-28 17:42:46 +0200

    solved exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    people.map((person) => {
+        if (typeof person.yearOfDeath == "undefined") {
+            person.yearOfDeath = new Date().getFullYear();
+    }});
+    people.sort((a, b) => {
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    });
+    return people[0]; 
 };
 
 // Do not edit below this line
