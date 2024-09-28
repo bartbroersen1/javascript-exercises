a276f328eadd09e7bcca446ed39574771d8d5320
Bart Broersen <143587354+bartbroersen1@users.noreply.github.com>
2024-09-28 15:20:24 +0200

    solved exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    
+    let array = [];
+    
+    books.map((item) => array.push(item.title));
+    return array;
 };
 
 // Do not edit below this line
