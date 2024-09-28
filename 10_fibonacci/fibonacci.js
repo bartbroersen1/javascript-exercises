347c81b76ceb64aa96d66798d58d681b86bf2df7
Bart Broersen <143587354+bartbroersen1@users.noreply.github.com>
2024-09-28 14:44:39 +0200

    solved exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) {return "OOPS"};
+    let array = [0, 1];
+    for (i = 0; i < (number); i++) {
+        let fibonacci = array[0 + i] + array[1 + i];
+        array.push(fibonacci);
+    };
+    return array[array.length - 2];
 };
 
 // Do not edit below this line
